264be7b83d39ec6827dbfe639db7f7b951ff4053
Rafal Labunski <rafallabunski@gmail.com>
2025-05-23 14:06:01 +0200

    Add Card and CardForm components, support adding cards and columns

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,38 @@
+import styles from './ColumnForm.module.scss';
+import { useState } from 'react';
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+
+const ColumnForm = props => {
+  const [title, setTitle] = useState('');
+  const [icon, setIcon] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.action({ title: title, icon: icon });
+    setTitle('');
+    setIcon('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.columnForm}>
+      <label>
+        <span>Title:</span>
+        <TextInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Icon:</span>
+        <TextInput
+          value={icon}
+          onChange={e => setIcon(e.target.value)}
+        />
+      </label>
+      <Button>Add column</Button>
+    </form>
+  );
+};
+
+export default ColumnForm;
