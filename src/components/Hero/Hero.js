4b4585dc66d84a4d2fe125d218c7af2e14323cbc
Rafal Labunski <rafallabunski@gmail.com>
2025-05-23 13:17:23 +0200

    Initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import './Hero.module.scss';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>My first React App</h1>
+      <p>A simple to-do app, with lists, columns and card</p>
+    </div>
+  );
+};
+
+export default Hero;
