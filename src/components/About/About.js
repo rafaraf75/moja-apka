be72c6c0262cd2003cd34d5773b5275c5db4b54c
Rafal Labunski <rafallabunski@gmail.com>
2025-09-04 00:28:41 +0200

    feat(views): add About and Favorite pages

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <section>
+    <h2>About</h2>
+    <p>To prosta aplikacja To-do. Więcej opisów wkrótce.</p>
+  </section>
+);
+
+export default About;
---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Favorite = () => (
+  <section>
+    <h2>Favorite</h2>
+    <p>Tu będą polubione karty.</p>
+  </section>
+);
+
+export default Favorite;
